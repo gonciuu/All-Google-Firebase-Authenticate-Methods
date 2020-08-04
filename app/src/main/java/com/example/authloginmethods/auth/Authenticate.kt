fc8a93976db13cb129e5d03af4030351961d0fd0
andryue <kacperwojak17@gmail.com>
2020-08-04 17:55:47 +0200

    create sign in anon methodd in Authience class

--- app/src/main/java/com/example/authloginmethods/auth/Authenticate.kt
+++ app/src/main/java/com/example/authloginmethods/auth/Authenticate.kt
@@ -1,6 +1,25 @@
 package com.example.authloginmethods.auth
 
+import android.content.Intent
+import android.util.Log
+import android.widget.Toast
+import com.example.authloginmethods.screens.MainActivity
+import com.example.authloginmethods.screens.ShowUserAuthDetails
+import com.google.firebase.auth.FirebaseAuth
+
 class Authenticate {
-    
+
+    private val auth = FirebaseAuth.getInstance()
+
+    fun signInAnomyus(activity:MainActivity){
+        auth.signInAnonymously().addOnCompleteListener {task->
+            if(task.isSuccessful){
+                activity.startActivity(Intent(activity.applicationContext,ShowUserAuthDetails::class.java))
+                Log.d("TAG",task.result!!.user.uid)
+            }
+            else Toast.makeText(activity.applicationContext,"Error when creating anon account",Toast.LENGTH_SHORT).show()
+        }
+    }
+
 
 }
--- app/src/main/java/com/example/authloginmethods/screens/MainActivity.kt
+++ app/src/main/java/com/example/authloginmethods/screens/MainActivity.kt
@@ -3,9 +3,13 @@ package com.example.authloginmethods.screens
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.authloginmethods.R
+import com.example.authloginmethods.auth.Authenticate
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val authenticate = Authenticate()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,7 +19,7 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         anomyusLogin.setOnClickListener {
-
+            authenticate.signInAnomyus(this)
         }
     }
 
