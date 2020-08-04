59942bfc3a0dc7f1e34705f126a44b57ecd0774a
andryue <kacperwojak17@gmail.com>
2020-08-04 17:47:07 +0200

    add firebase auth to app

--- app/src/main/java/com/example/authloginmethods/auth/Authenticate.kt
+++ app/src/main/java/com/example/authloginmethods/auth/Authenticate.kt
@@ -1,5 +1,6 @@
 package com.example.authloginmethods.auth
 
 class Authenticate {
+    
 
 }
--- app/src/main/java/com/example/authloginmethods/screens/MainActivity.kt
+++ app/src/main/java/com/example/authloginmethods/screens/MainActivity.kt
@@ -3,10 +3,20 @@ package com.example.authloginmethods.screens
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.authloginmethods.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+    override fun onResume() {
+        super.onResume()
+        anomyusLogin.setOnClickListener {
+
+        }
+    }
+
 }
