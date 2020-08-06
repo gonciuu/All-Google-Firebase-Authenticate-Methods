b4f7040a8bb263a5e7eccfe8f63a5888e1a72e59
andryue <kacperwojak17@gmail.com>
2020-08-06 10:51:48 +0200

    create login and register method with email and password

--- app/src/main/java/com/example/authloginmethods/auth/Authenticate.kt
+++ app/src/main/java/com/example/authloginmethods/auth/Authenticate.kt
@@ -58,4 +58,35 @@ class Authenticate(private val fragment: Fragment) {
     //===========================================================================================
 
 
+    //--------------login with email and password--------------
+
+    fun loginWithEmailAndPassword(email:String,password:String){
+        auth.signInWithEmailAndPassword(email,password).addOnCompleteListener {task->
+            if (task.isSuccessful) {
+                val user = task.result!!.user
+                val listOfInfo = arrayListOf<String>(user.uid, if(!user.displayName.isNullOrEmpty()) user.displayName!! else "No display name",
+                    if(!user.displayName.isNullOrEmpty()) user.phoneNumber!! else "No phone number", if(!user.email.isNullOrEmpty()) user.email!! else "No email")
+                userDetailsViewModel.setInfo(listOfInfo)//set info about user account
+                fragment.findNavController().navigate(R.id.action_loginMethodsFragment_to_userDetailsFragment)//navigate to fragment
+                Log.d("TAG",user.uid)
+            } else Toast.makeText(fragment.context, "Error when creating anon account", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    //--------------register with email and password--------------
+    fun registerWithEmailAndPassword(email:String,password:String){
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener {task->
+            if (task.isSuccessful) {
+                val user = task.result!!.user
+                val listOfInfo = arrayListOf<String>(user.uid, if(!user.displayName.isNullOrEmpty()) user.displayName!! else "No display name",
+                    if(!user.displayName.isNullOrEmpty()) user.phoneNumber!! else "No phone number", if(!user.email.isNullOrEmpty()) user.email!! else "No email")
+                userDetailsViewModel.setInfo(listOfInfo)//set info about user account
+                fragment.findNavController().navigate(R.id.action_loginMethodsFragment_to_userDetailsFragment)//navigate to fragment
+                Log.d("TAG",user.uid)
+            } else Toast.makeText(fragment.context, "Error when creating anon account", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+
 }
