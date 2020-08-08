ebfc9ef591ed9c5e29e5a909e142dadbc279c076
andryue <kacperwojak17@gmail.com>
2020-08-08 21:15:12 +0200

    create basic of phone number verificaition function

--- app/src/main/java/com/example/authloginmethods/auth/Authenticate.kt
+++ app/src/main/java/com/example/authloginmethods/auth/Authenticate.kt
@@ -7,8 +7,14 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.authloginmethods.R
 import com.example.authloginmethods.screens.view_models.UserDetailsViewModel
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
 
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthProvider
+import java.util.concurrent.TimeUnit
 
 class Authenticate(private val fragment: Fragment) {
 
@@ -60,8 +66,71 @@ class Authenticate(private val fragment: Fragment) {
     //-------------------------------Login with phone number-----------------------------
     fun loginWithPhoneNumber(phoneNumber:String){
 
+        var storedVerificationId = ""
+        var resendToken: PhoneAuthProvider.ForceResendingToken
+
+
+        val callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+                Log.d("TAG", "onVerificationCompleted:$credential")
+                signInWithPhoneAuthCredential(credential)
+            }
+
+            override fun onVerificationFailed(e: FirebaseException) {
+                // This callback is invoked in an invalid request for verification is made,
+                // for instance if the the phone number format is not valid.
+                Log.w("TAG", "onVerificationFailed", e)
+            }
+
+            override fun onCodeSent(
+                verificationId: String,
+                token: PhoneAuthProvider.ForceResendingToken
+            ) {
+                // The SMS verification code has been sent to the provided phone number, we
+                // now need to ask the user to enter the code and then construct a credential
+                // by combining the code with a verification ID.
+                Log.d("TAG", "onCodeSent:$verificationId")
+
+                // Save verification ID and resending token so we can use them later
+                storedVerificationId = verificationId
+                resendToken = token
+
+
+
+                // ...
+            }
+        }
+
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+            phoneNumber, // Phone number to verify
+            60, // Timeout duration
+            TimeUnit.SECONDS, // Unit of timeout
+            fragment.requireActivity(), // Activity (for callback binding)
+            callbacks) // OnVerificationStateChangedCallbacks
+
+        auth.setLanguageCode("pl")
+
+        Log.d("TAG",phoneNumber)
     }
     //===================================================================================
 
+    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("TAG", "signInWithCredential:success")
+
+                    val user = task.result?.user
+                } else {
+                    // Sign in failed, display a message and update the UI
+                    Log.w("TAG", "signInWithCredential:failure", task.exception)
+                    if (task.exception is FirebaseAuthInvalidCredentialsException) {
+
+                    }
+                }
+            }
+    }
 
 }
