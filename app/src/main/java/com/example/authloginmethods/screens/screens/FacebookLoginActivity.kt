28d1474ce29fe2eb0b39fe87660a2a30ab8a9750
andryue <kacperwojak17@gmail.com>
2020-08-13 09:12:45 +0200

    setup gradle, strings and add new activity

---
+++ app/src/main/java/com/example/authloginmethods/screens/screens/FacebookLoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.authloginmethods.screens.screens
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.authloginmethods.R
+
+class FacebookLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_facebook_login)
+    }
+}
