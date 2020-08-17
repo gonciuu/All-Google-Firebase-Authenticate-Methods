c068375f159d02ce732f5f9146e27929bfe0e29d
andryue <kacperwojak17@gmail.com>
2020-08-17 09:29:05 +0200

    final touches

--- app/src/main/java/com/example/authloginmethods/screens/screens/LoginMethodsFragment.kt
+++ app/src/main/java/com/example/authloginmethods/screens/screens/LoginMethodsFragment.kt
@@ -74,6 +74,8 @@ class LoginMethodsFragment : Fragment() {
         githubLogin.setOnClickListener {
             findNavController().navigate(R.id.action_loginMethodsFragment_to_loginWithGithub)
         }
+
+
         //login with microsoft
         microsoftLogin.setOnClickListener {
             val provider = OAuthProvider.newBuilder("microsoft.com")
